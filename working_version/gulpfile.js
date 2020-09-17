076357082532a46de6b90b03034412231ec125ca
Yaroslav <doctor355@yahoo.com>
2020-09-17 12:43:45 +0300

    added normal css and some gulp func

--- working_version/gulpfile.js
+++ working_version/gulpfile.js
@@ -1,6 +1,33 @@
-function defaultTask(cb) {
-  console.log("Hello GULP!");
-  cb();
-}
+// function defaultTask(cb) {
+//   console.log("Hello GULP!");
+//   cb();
+// }
 
-exports.default = defaultTask;
+// exports.default = defaultTask;
+// var gulp = require("gulp");
+
+// gulp.task("hello", function () {
+//   console.log("Hello Zell");
+// });
+
+// const { series } = require("gulp");
+
+// function hello(cb) {
+//   console.log("Hello - it is Gulp 4");
+//   cb();
+// }
+
+// function hi(cb) {
+//   console.log("Hi - other function GULP-4");
+//   cb();
+// }
+// exports.default = series(hello, hi);
+// exports.hello = hello;
+// exports.hi = hi;
+
+
+var gulp = require("gulp");
+
+gulp.task("test", function () {
+  console.log("Я работаю");
+});
